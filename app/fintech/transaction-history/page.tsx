8ed0913e8654e07b8a708d78512f42992de28d5d
scriptpythonic <Quadribasit04@gmail.com>
2025-02-19 18:42:09 +0100

    still making some ammendment

--- app/fintech/transaction-history/page.tsx
+++ app/fintech/transaction-history/page.tsx
@@ -29,7 +29,7 @@ const TransactionContent = () => {
      <div className="flex gap-4">
        <Bell
          className="w-6 h-6 text-gold cursor-pointer"
-         onClick={() => router.push("/notifications")}
+         onClick={() => router.push("/fintech/notifications")}
        />
 
        <MdAddCard className="w-6 h-6 cursor-pointer text-gold" />
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const App =()=>{
+  return(
+    <div>
+      <h1>App name </h1>
+    </div>
+  )
+}
+
+export default App;
