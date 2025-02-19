87bc8a570487c36af9e2a0c13551f9a6f84b48a3
scriptpythonic <Quadribasit04@gmail.com>
2025-02-19 20:25:17 +0100

    message

--- app/constant/Header.tsx
+++ app/constant/Header.tsx
@@ -1,11 +1,20 @@
 'use client';
-import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import { Menu, X } from 'lucide-react';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export default function Header() {
+  const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
+
+  const navLinks = [
+    { name: 'Puddle', path: '/puddle' },
+    { name: 'App store', path: '/store' },
+    { name: 'Logistics', path: '/logistics' },
+    { name: 'Fintech', path: '/fintech' },
+  ];
 
   return (
     <header className="w-full font-sarala fixed top-0 left-0 right-0 backdrop-blur-sm bg-[#100F0DB2] bg-opacity-70 z-50 px-8 py-4">
@@ -19,10 +28,17 @@ export default function Header() {
         {/* Centered Desktop Navigation */}
         <div className="hidden md:flex flex-grow z-50 justify-center ">
           <nav className="flex gap-x-7 py-2 px-8 bg-[#080604] rounded-lg">
-            <a href="#" className="text-[#C3AD60] font-bold">Puddle</a>
-            <a href="#" className="text-[#C3AD60] font-bold">App store</a>
-            <a href="#" className="text-[#C3AD60] font-bold">Logistics</a>
-            <a href="#" className="text-white font-bold">Fintech</a>
+            {navLinks.map((link) => (
+              <a
+                key={link.path}
+                href={link.path}
+                className={`font-bold ${
+                  pathname === link.path ? 'text-white' : 'text-[#C3AD60]'
+                }`}
+              >
+                {link.name}
+              </a>
+            ))}
           </nav>
         </div>
 
@@ -45,10 +61,18 @@ export default function Header() {
           className="md:hidden absolute top-full left-0 w-full z-[100] bg-[#100F0DB2] backdrop-blur-md text-left py-5"
         >
           <ul className="flex flex-col gap-4 h-[100vh] px-8">
-            <li><a href="#" className="text-[#C3AD60] font-bold">Puddle</a></li>
-            <li><a href="#" className="text-[#C3AD60] font-bold">App store</a></li>
-            <li><a href="#" className="text-[#C3AD60] font-bold">Logistics</a></li>
-            <li><a href="#" className="text-white font-bold">Fintech</a></li>
+            {navLinks.map((link) => (
+              <li key={link.path}>
+                <a
+                  href={link.path}
+                  className={`font-bold ${
+                    pathname === link.path ? 'text-white' : 'text-[#C3AD60]'
+                  }`}
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </motion.div>
       )}
