7c84ff8926468adaa6fc23af1aa2fab752e81507
scriptpythonic <Quadribasit04@gmail.com>
2025-02-25 09:37:49 +0100

    make change on the eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,15 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off" // ✅ Disable React-in-JSX-Scope rule
+    }
+  }
 ];
